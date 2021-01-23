f65bceafac97339dfe341a8eca094ede1931a33a
wty262690 <71436111+wty262690@users.noreply.github.com>
2021-01-23 20:16:27 +0800

    page_5

--- src/views/example5.vue
+++ src/views/example5.vue
@@ -2,22 +2,18 @@
 <div class="page5" >
     <span unselectable="on" >
     <div class="title">年節親戚聊天專用指南</div>
-    
-    <div class="contain">
+    <div class="pword">
         <div class="bigitem ">話題預測</div>
-        <div class="item">CVOID19</div>
-        <div class="item">政治</div>
-        <div class="item">股票</div>
-        <div class="item">房市</div>
-        <div class="item">工作</div>
-        <div class="item">學校</div>
-        <div class="item">婚嫁</div>
-        <i class="fa fa-plus" aria-hidden="true"></i>
-      </div>
-
+        <div class="contain"  v-for="item in newitem" :key="item">
+            <div class="item">{{item.new}}</div>
+          </div>
+          <i class="fa fa-plus" aria-hidden="true"></i>
+    </div>
+    <div class="yword">
         <div class="bigitem">年前準備</div>
             <el-progress type="dashboard" :percentage="percentage" :color="colors"></el-progress>
             <div>
+              <p class="final">完成度</p>
             <el-button-group>
                 <el-button icon="fa fa-chevron-down" @click="decrease"></el-button>
                 <el-button icon="fa fa-chevron-up" @click="increase"></el-button>
@@ -31,14 +27,14 @@
             <a href="https://www.youtube.com/user/NTDAPTV">新唐人 </a>
             <a href="https://www.youtube.com/channel/UCTqPBBnP2T57kmiPQ87986g">眼球中央電視台 </a>
             <a href="https://www.youtube.com/user/twnexttv">壹電視 </a>
-            <a href="https://www.youtube.com/channel/UCpu3bemTQwAU8PqM4kJdoEQ">中天</a>
+            <a href="https://www.youtube.com/channel/UCpu3bemTQwAU8PqM4kJdoEQ">中天 </a>
             <a href="https://www.youtube.com/user/TVBS">TVBS </a>
             <a href="https://www.youtube.com/user/newsebc">東森 </a>
             <a href="https://www.youtube.com/user/chinatvnews">中視 </a>
             <a href="https://www.youtube.com/user/ttvnewsview">台視 </a>
             <a href="https://www.youtube.com/user/FTVCP">民視 </a>
             </div>
-    
+    </div>
     </span>
 </div>
 </template>
@@ -47,10 +43,23 @@
 <script>
   export default {
     data() {
+      var countDownDate = new Date("Feb 10, 2021 0:0:0").getTime();
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       return {
-        percentage: 50,
+        percentage: 100-days,
         colors: [
-          {color: 'red', percentage: 20},
+          {color: 'rgb(192,44,56)', percentage: 20},
+        ],
+        newitem: [
+          {new: "CVOID19",},
+          {new: "政治",},
+          {new: "股票",},
+          {new: "房市",},
+          {new: "工作",},
+          {new: "學校",},
+          {new: "婚嫁",},
         ]
       };
     },
@@ -85,17 +94,24 @@
 }
 .title{
     padding-top:10vh;
+    
     width:100vw;
     font-size:70px;
     font-family:"cat";
 }
 .smallitem{
+    padding-top:10vh;
     font-size:20px;
+    line-height: 40px;
 }
 .item{
+    text-align: center;
     color:white;
 }
 .bigitem{
+    text-align:center;
+    padding-top:100px;
+    padding-bottom:30px;
     font-size:30px;
     font-family:"normalc";
     color:white;
@@ -103,14 +119,18 @@
 .contain{
     font-size:18px;
     text-align: left;
-    padding:10px;
-    padding-left:10vw;
+    padding-top:5px;
+    padding-bottom:20px;
 }
 .el-button{
   color:white;
   background-color:rgba(0, 0, 0, 0);
   border:0;
-  padding:9px ;
+  padding:5px ;
+  padding-top:0px;
+}
+.el-progress{
+  top:20px;
 }
 .el-progress__text{
   color:white;
@@ -120,5 +140,28 @@
 a{
   color:white;
   text-decoration: none;
+}
+.smallitem a:hover{
+  border-bottom:1px solid white;
+}
+.pword{
+  position:absolute;
+  width:50vw;
+  vertical-align: middle;
+}
+.yword{
+  position:absolute;
+  left:50vw;
+  width:40vw;
+  vertical-align: middle;
+}
+i{
+  color:white;
+  font-size:19px;
+}
+.final{
+  margin:5px;
+  font-size:17px;
+  color:white;
 }
 </style>
